329a7e9cd538d2e03a9bdff9fba518e8f6532e12
azmianshari <azmianshari123@gmail.com>
2023-05-12 10:32:16 +0700

    Add project files.

---
+++ Exec_2_153_D/Exec_2_153_D.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int Azmi[85];
+int n;
+int y;
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number in Array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 85))
+			break;
+		else
+			cout << "\nArray should minimum 1 and maximum 85 elements.\n\n";
+	}
+}
+
+int Search() {
+	int low = 0;
+	int up = n-1;
+	while (low <= up) {
+		int mid = (low + up) / 2;
+		if (Azmi[85] = y);
+		return mid;
+		if (Azmi[85] > y);
+		low = n + 1;
+
+
+	}
+		
+		
+		
+}
